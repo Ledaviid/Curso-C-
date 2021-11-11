f665128ed21f79d72bb320a0d4e637c741b0cb98
Davidtpz <78239912+Ledaviid@users.noreply.github.com>
2021-11-11 15:44:33 -0600

    Add files via upload
    Un sencillo ejemplo de que es una funcion y como se utiliza.

---
+++ Que es una funcion.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int suma(int num1, int num2){
+	int resultado;
+	resultado= num1+num2;
+	return resultado;
+}
+
+
+int main() {
+	int a,b,c;
+	printf("ingresa valores a y b");
+	scanf("%d",&a);
+	scanf("%d",&b);
+ 
+	c=suma(a,b);
+ 
+printf("%d",c);	
+	return 0;
+}
+
