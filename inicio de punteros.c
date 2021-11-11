bb477c716b0517638860ba4c038f4950534141d3
Davidtpz <78239912+Ledaviid@users.noreply.github.com>
2021-11-11 15:03:12 -0600

    Add files via upload
    Ejemplo para iniciarse en punteros

---
+++ inicio de punteros.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+
+int num;
+num = 10;
+int *puntero=NULL; /*No confundir esto declara que es un tipo puntero*/
+puntero=&num;
+
+*puntero=*puntero+1; /*esto es igual hacer num=num+1*/
+
+printf("%d",num);
+/*Ya sea con *puntero o num es la misma celda de memoria y obtiene el mismo valor*/
+
+	
+	return 0;
+}
+
